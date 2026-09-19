d60e6a56082f7a63171db106215ca2cbb2b65832
Sasasyyy <2410631170027@student.unsika.ac.id>
2026-09-19 19:01:37 +0700

    [praktikum 6]: Model Eloquent dan Relasi Antar Tabel

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -1,21 +1,20 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
-
+{
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-         $categories = ['Sembako', 'Minuman', 'Makanan Ringan', 'Kebutuhan Rumah Tangga'];
-    foreach ($categories as $name) {
-        Category::create(['name' => $name]);
-
-   
+        $categories = ['Sembako', 'Minuman', 'Makanan Ringan', 'Kebutuhan Rumah Tangga'];
+        foreach ($categories as $name) {
+            Category::create(['name' => $name]);
+        }
     }
 }
